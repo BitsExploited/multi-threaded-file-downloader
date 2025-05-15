ed6ed9c21be166ea1f8e8f4777425301279711be
BitsExploited <tspamiitesh@gmail.com>
2025-05-15 06:29:09 +0530

    Init the project

--- main.go
+++ main.go
@@ -33,5 +33,28 @@ type ProgressWriter struct {
 }
 
 func main() {
+	if len(os.Args) < 3 {
+		fmt.Println("Usage: donwloader <url> <output_path> [concurrency]")
+		os.Exit(1)
+	}
 
+	url := os.Args[1]
+	outPath := os.Args[2]
+	concurrecny := 4
+	if len(os.Args) > 3 {
+		if c, err := strconv.Atoi(os.Args[3]); err == nil && c > 0 {
+			concurrecny = c
+		}
+	}
+
+	downloader := &Downloader{
+		URL: url,
+		OutPath: outPath,
+		Concurrency: concurrecny,
+	}
+
+	if err := downloader.Download(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
+	}
 }
